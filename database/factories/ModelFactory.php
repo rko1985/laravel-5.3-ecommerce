6c3965beee694274e93699b43bf5f31ef95f3aae
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-11-15 08:28:16 -1000

    installed front end layout template

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,13 +12,13 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(App\User::class, function (Faker\Generator $faker) {
-    static $password;
+$factory->define(App\Product::class, function (Faker\Generator $faker) {
+    
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'name' => $faker->sentence(3),
+        'image' => 'uploads/products/book.jpg',
+        'description' => $faker->paragraph(4),
+        'price' => $faker->numberBetween(100,10000)
     ];
 });
--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -12,21 +12,7 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        $p1 = [
-            'name' => 'Learn to build websites in HTML5',
-            'image' => 'uploads/products/book.jpg',
-            'price' => '5000',
-            'description' => 'some description here....'
-        ];
-
-        $p2 = [
-            'name' => 'Learn to build websites in CSS',
-            'image' => 'uploads/products/book.jpg',
-            'price' => '2500',
-            'description' => 'some description here....'
-        ];
-
-        Product::create($p1);
-        Product::create($p2);
+        factory(\App\Product::class, 30)->create();
+        
     }
 }
